06c48d2737e0750a963993484fe8db60cc98776d
David-McLaughlin <108822713+David-McLaughlin@users.noreply.github.com>
2023-07-04 09:08:53 -0400

    Update stockValueYearMon.py

--- StockAnalysis/stockValueYearMon.py
+++ StockAnalysis/stockValueYearMon.py
@@ -1,18 +1,25 @@
-#######################################################################################
-# Function takes year and ticker as input and returns a dataframe of the stock detail #
-#######################################################################################
+
+####################
+# Import Libraries #
+####################
 import yfinance as yf
 import pandas as pd
 
-def getAnnualStkData(year, ticker):
-    stock = yf.Ticker(ticker)
-    ############################
-    # Get 28 Day Month February
-    ############################
+#############################
+# Configure Ticker and Year #
+#############################
+ticker='BB'
+year='2023'
+stock = yf.Ticker(ticker)
 
-    data = yf.download(ticker, start=str(year) + str('-01-01'),
-                   end=str(year) + str('-12-31'), group_by='tickers', progress=False, header=None)
-    df = pd.DataFrame(
-         data, columns=['Open', 'Close'])
-    #print(df)
-    return df
+####################
+# Pull Stock Data  #
+####################
+data = yf.download(ticker, start=str(year) + str('-01-01'),
+              end=str(year) + str('-12-31'), group_by='tickers', progress=False)
+df = pd.DataFrame(
+     data, columns=['Open', 'Close'])
+####################
+# Print Ticker Data#
+####################
+print(df)
